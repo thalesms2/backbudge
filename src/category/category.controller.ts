0dd8c72a9f5316b56e4e547fd3a4c0511dd685df
Thales Minoru Sato <thalesms2@outlook.com>
2025-01-12 20:39:00 -0300

    fi: typo

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { ControllerFactory } from 'services/factory/controller.factory';
 import { CategoryService } from './category.service';
-import { ICategory } from './interface/Category.interface';
+import { ICategory } from './interface/category.interface';
 import { CategoryDto } from './interface/category.dto';
 
 @Controller('Category')
